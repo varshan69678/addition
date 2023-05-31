28a1211324a7c84c05eee67b75c846e6c0f2da9e
varshan69678 <varshan69678@gmail.com>
2023-05-31 15:08:42 +0530

    this is addition code

---
+++ src/com/sathya/add/Add.java
@@ -0,0 +1,10 @@
+package com.sathya.add;
+
+public class Add {
+	public static void main(String args[]) {
+		int num1=10,num2=30;
+		int add= num1+num2;
+		System.out.println( "Addition of two numbers:"+add);
+		
+	}
+}
